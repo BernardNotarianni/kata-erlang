570aeeca5ecf51320f172f7056262e81bd27673e
Bernard Notarianni <bernard.notarianni@gmail.com>
2015-03-16 18:35:15 +0100

    Ajoute score de bowling

---
+++ src/bowling.erl
@@ -0,0 +1,16 @@
+-module (bowling).
+
+-export ([score/1]).
+
+score ([]) -> 0;
+
+score ([R1, R2, B]) when R1+R2=:=10 -> R1 + R2 + B;
+score ([R1, R2, B | Other]) when R1+R2=:=10 -> R1 + R2 + B + score([B | Other]);
+
+score ([10, T1, T2]) -> 10 + T1 + T2;
+score ([10, T1, T2 | Other]) -> 10 + T1 + T2 + score ([T1,T2|Other]);
+
+score ([H|T]) -> H + score(T).
+
+
+
--- tdd.sh
+++ tdd.sh
@@ -5,16 +5,12 @@ function ctrl_c {
     exit
 }
 
-
 trap ctrl_c INT TERM EXIT
 
 while true; do
-    echo ""
 
     rebar compile eunit skip_deps=true
-
     echo ""
-    date +"%k:%M:%S - Waiting for source modification..."
 
     inotifywait -qr -e modify -e create -e move -e delete src test
 done
